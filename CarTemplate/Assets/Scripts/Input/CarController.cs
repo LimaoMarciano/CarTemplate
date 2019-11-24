94c4566c3ca56a9c7fcbf5c3e4bc1e09ea1282b1
Marcelo Silva <marceloapsilva@gmail.com>
2019-11-24 20:11:06 -0300

    Added debug UI for accelerator, brakes and clutch; Added input filtering on car controller;

--- CarTemplate/Assets/Scripts/Input/CarController.cs
+++ CarTemplate/Assets/Scripts/Input/CarController.cs
@@ -7,32 +7,49 @@ public class CarController : MonoBehaviour
 {
 
     public Car car;
+    public float changeGearSpeed = 10f;
+    public AnimationCurve acceleratorFilter;
+    public AnimationCurve brakesFilter;
+    public AnimationCurve steeringFilter;
+
+    public bool useFilteredAccelerator = false;
+    public bool useFilteredBrakes = false;
+    public bool useFilteredSteering = false;
+    bool isAcceleratorEnabled = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
-
         float handbrakeInput = 0f;
 
         if (Input.GetButtonDown("Fire1"))
         {
+            
+            if (car.gearbox.CurrentGear != -1)
+            {
+                StopCoroutine(ChangeGear());
+                StartCoroutine(ChangeGear());
+            }
             car.gearbox.IncreaseGear();
-            //StopCoroutine(changeGearCoroutine);
-            //StartCoroutine(changeGearCoroutine);
+
         }
 
         if (Input.GetButtonDown("Fire3"))
         {
+            
+            if (car.gearbox.CurrentGear != -1)
+            {
+                StopCoroutine(ChangeGear());
+                StartCoroutine(ChangeGear());
+            }
             car.gearbox.DecreaseGear();
-            //StopCoroutine(changeGearCoroutine);
-            //StartCoroutine(changeGearCoroutine);
 
         }
 
@@ -41,8 +58,67 @@ public class CarController : MonoBehaviour
             handbrakeInput = 1f;
         }
 
-        car.engine.acceleratorInput = Input.GetAxis("RightTrigger");
-        car.steering.SetSteeringInput(Input.GetAxis("Horizontal"));
-        car.brakes.ApplyPressure(Input.GetAxis("LeftTrigger"), handbrakeInput);
+        if (isAcceleratorEnabled)
+        {
+            if (useFilteredAccelerator)
+            {
+                car.engine.acceleratorInput = GetFilteredAcceleratorInput(Input.GetAxis("RightTrigger"));
+            }
+            else
+            {
+                car.engine.acceleratorInput = Input.GetAxis("RightTrigger");
+            }
+        }
+
+        if (useFilteredSteering)
+        {
+            car.steering.SetSteeringInput(GetFilteredSteerInput(Input.GetAxis("Horizontal")));
+        }
+        else
+        {
+            car.steering.SetSteeringInput(Input.GetAxis("Horizontal"));
+        }
+
+        if (useFilteredBrakes)
+        {
+            float filteredInput = GetFilteredBrakeInput(Input.GetAxis("LeftTrigger"));
+            car.brakes.ApplyPressure(filteredInput, handbrakeInput);
+        }
+        else
+        {
+            car.brakes.ApplyPressure(Input.GetAxis("LeftTrigger"), handbrakeInput);
+        }
+        
     }
+
+    IEnumerator ChangeGear()
+    {
+        isAcceleratorEnabled = false;
+        car.engine.acceleratorInput = 0f;
+        for (float clutchInput = 1f; clutchInput >= 0; clutchInput -= changeGearSpeed * Time.deltaTime)
+        {
+            car.clutch.clutchInput = clutchInput;
+            yield return null;
+        }
+
+        car.clutch.clutchInput = 0;
+        isAcceleratorEnabled = true;
+
+    }
+
+    private float GetFilteredAcceleratorInput (float input)
+    {
+        return Mathf.Clamp01(acceleratorFilter.Evaluate(input));
+    }
+
+    private float GetFilteredBrakeInput (float input)
+    {
+        return Mathf.Clamp01(brakesFilter.Evaluate(input));
+    }
+
+    private float GetFilteredSteerInput (float input)
+    {
+        return Mathf.Clamp(input, -1, 1);
+    }
+
 }
