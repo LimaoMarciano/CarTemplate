a3dd29b1e3c601f178a4302b7994c58ea7a6bf7e
Marcelo Silva <marceloapsilva@gmail.com>
2020-04-05 18:40:28 -0300

    - Improved abs/brakes debug UI

--- CarTemplate/Assets/CarTemplate/Scripts/UI/AbsDebug.cs
+++ CarTemplate/Assets/CarTemplate/Scripts/UI/AbsDebug.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CarTemplate;
 
 public class AbsDebug : MonoBehaviour
 {
+    public Car car;
     public CarController carController;
-    public Fillbar FLAbsGauge;
-    public Fillbar FRAbsGauge;
-    public Fillbar RLAbsGauge;
-    public Fillbar RRAbsGauge;
+    public DoubleFillbar FLAbsGauge;
+    public DoubleFillbar FRAbsGauge;
+    public DoubleFillbar RLAbsGauge;
+    public DoubleFillbar RRAbsGauge;
 
     // Start is called before the first frame update
     void Start()
     {
+        FLAbsGauge.SetLabel("FL");
+        FRAbsGauge.SetLabel("FR");
+        RLAbsGauge.SetLabel("RL");
+        RRAbsGauge.SetLabel("RR");
+
         FLAbsGauge.SetMaxValue(1f);
         FRAbsGauge.SetMaxValue(1f);
         RLAbsGauge.SetMaxValue(1f);
@@ -22,9 +29,36 @@ public class AbsDebug : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FLAbsGauge.value = carController.abs.FrontLeftAbs.AllowedBrakePressure;
-        FRAbsGauge.value = carController.abs.FrontRightAbs.AllowedBrakePressure;
-        RLAbsGauge.value = carController.abs.RearLeftAbs.AllowedBrakePressure;
-        RRAbsGauge.value = carController.abs.RearRightAbs.AllowedBrakePressure;
+        float FLPressure = car.brakes.FrontLeftBrakeTorque / car.brakes.data.frontBrakeTorque;
+        float FRPressure = car.brakes.FrontRightBrakeTorque / car.brakes.data.frontBrakeTorque;
+        float RLPressure = car.brakes.RearLeftBrakeTorque / car.brakes.data.rearBrakeTorque;
+        float RRPressure = car.brakes.RearRightBrakeTorque / car.brakes.data.rearBrakeTorque;
+
+        FLAbsGauge.value01 = FLPressure;
+        FRAbsGauge.value01 = FRPressure;
+        RLAbsGauge.value01 = RLPressure;
+        RRAbsGauge.value01 = RRPressure;
+
+        FLAbsGauge.value02 = GetAbsValue(carController.abs.FrontLeftAbs) * FLPressure;
+        FRAbsGauge.value02 = GetAbsValue(carController.abs.FrontRightAbs) * FRPressure;
+        RLAbsGauge.value02 = GetAbsValue(carController.abs.RearLeftAbs) * RLPressure;
+        RRAbsGauge.value02 = GetAbsValue(carController.abs.RearRightAbs) * RRPressure;
     }
+
+    private float GetAbsValue (AntiLockBrakeSensor absSensor)
+    {
+        float value;
+
+        if (absSensor.IsActive)
+        {
+            value = absSensor.AllowedBrakePressure;
+        }
+        else
+        {
+            value = 0;
+        }
+
+        return value;
+    }
+
 }
---
+++ CarTemplate/Assets/CarTemplate/Scripts/UI/DoubleFillbar.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoubleFillbar : MonoBehaviour
+{
+    public Text labelText;
+    public Image fill01;
+    public Image fill02;
+    public Color fillColor01 = Color.blue;
+    public Color fillColor02 = Color.cyan;
+    public Color outOfRangeColor = Color.red;
+
+    private float maxValue = 1f;
+    public float value01 = 0f;
+    public float value02 = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        fill01.color = fillColor01;
+        fill02.color = fillColor02;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fillAmount = (value01 / maxValue);
+
+        if (fillAmount > 1)
+        {
+            fill01.color = outOfRangeColor;
+            fillAmount = 1;
+        }
+        else
+        {
+            fill01.color = fillColor01;
+        }
+
+        fill01.fillAmount = fillAmount;
+
+        fillAmount = (value02 / maxValue);
+
+        if (fillAmount > 1)
+        {
+            fill02.color = outOfRangeColor;
+            fillAmount = 1;
+        }
+        else
+        {
+            fill02.color = fillColor02;
+        }
+
+        fill02.fillAmount = fillAmount;
+
+    }
+
+    public void SetLabel(string label)
+    {
+        labelText.text = label;
+    }
+
+    public void SetMaxValue(float max)
+    {
+        maxValue = max;
+    }
+}
